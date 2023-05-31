7c6111cbed27dc9b645184baae6caacb2e592a9a
robreno <robreno@hotmail.com>
2023-05-31 07:43:16 -0700

    Comments.

--- UBViews.Maui/Services/IRepositoryService.cs
+++ UBViews.Maui/Services/IRepositoryService.cs
@@ -33,86 +33,86 @@ namespace UBViews.Services
         /// SaveQueryResultAsync
         /// </summary>
         /// <param name="queryResult"></param>
-        /// <returns></returns>
+        /// <returns>int success flag</returns>
         Task<int> SaveQueryResultAsync(QueryResult queryResult);
 
         /// <summary>
         /// QueryResultExistsAsync
         /// </summary>
         /// <param name="queryString"></param>
-        /// <returns></returns>
+        /// <returns>(bool, int) tuple</returns>
         Task<(bool, int)> QueryResultExistsAsync(string queryString);
 
         /// <summary>
         /// GetQueryResultsAsync
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List of QueryResult</returns>
         Task<List<QueryResult>> GetQueryResultsAsync();
 
         /// <summary>
         /// GetQueryResultByIdAsync
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <returns>QueryResultDto</returns>
         Task<QueryResultDto> GetQueryResultByIdAsync(int id);
 
         /// <summary>
         /// GetQueryResultByQueryStringAsync
         /// </summary>
         /// <param name="queryString"></param>
-        /// <returns></returns>
+        /// <returns>QueryResultDto</returns>
         Task<QueryResultDto> GetQueryResultByQueryStringAsync(string queryString);
 
         /// <summary>
         /// GetTermOccurrencesAsync
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List of TermOccurrence</returns>
         Task<List<TermOccurrence>> GetTermOccurrencesAsync();
 
         /// <summary>
         /// GetTermOccurrencesByQueryResultIdAsync
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <returns>List of TermOccurrence</returns>
         Task<List<TermOccurrence>> GetTermOccurrencesByQueryResultIdAsync(int id);
 
         /// <summary>
         /// GetQueryCommandsAsync
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List of QueryCommandDto</returns>
         Task<List<QueryCommandDto>> GetQueryCommandsAsync();
 
         /// <summary>
         /// GetPostingsAsync
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List of PostingList</returns>
         Task<List<PostingList>> GetPostingsAsync();
 
         /// <summary>
         /// GetPostingByLexemeAsync
         /// </summary>
         /// <param name="lexeme"></param>
-        /// <returns></returns>
+        /// <returns>PostingList</returns>
         Task<PostingList> GetPostingByLexemeAsync(string lexeme);
 
         /// <summary>
         /// GetTokenOccurrencesAsync
         /// </summary>
         /// <param name="postingId"></param>
-        /// <returns></returns>
+        /// <returns>List of TokenOccurrence</returns>
         Task<List<TokenOccurrence>> GetTokenOccurrencesAsync(int postingId);
 
         /// <summary>
         /// GetTokenStemsAsync
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List of TokenStem</returns>
         Task<List<TokenStem>> GetTokenStemsAsync();
 
         /// <summary>
         /// GetTokenStemAsync
         /// </summary>
         /// <param name="lexeme"></param>
-        /// <returns></returns>
+        /// <returns>TokenStem</returns>
         Task<TokenStem> GetTokenStemAsync(string lexeme);
         #endregion
     }
