43d3199f0c1097858bc91c23c188bb6d864b263c
robreno <robreno@hotmail.com>
2023-09-21 16:24:40 -0700

    .

--- UBViews.Maui/Models/Query/TokenOccurrenceComparer.cs
+++ UBViews.Maui/Models/Query/TokenOccurrenceComparer.cs
@@ -8,11 +8,11 @@ namespace UBViews.Models.Query;
 
 public class TokenOccurrenceComparer
 {
+    public string Term { get; set; }
     public int DocumentId { get; set; }
     public int SequenceId { get; set; }
     public int DocumentPosition { get; set; }
     public int TextPosition { get; set; }
     public int TextLength { get; set; }
-    public string Term { get; set; }
     public string ParagraphId { get; set; }
 }
