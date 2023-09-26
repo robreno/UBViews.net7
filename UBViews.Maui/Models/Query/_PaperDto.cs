82ba00d683f4d28f93196aa4b205bc0115af7f1f
robreno <robreno@hotmail.com>
2023-09-26 06:45:06 -0700

    Excluded from project.

---
+++ UBViews.Maui/Models/Query/_PaperDto.cs
@@ -0,0 +1,12 @@
+﻿namespace UBViews.Models.Query;
+public class PaperDto
+{
+    public int Id { get; set; }
+    public bool ScrollTo { get; set; }
+    public string Uid { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int PartId { get; set; }
+    public string PartTitle { get; set; }
+    public string TimeSpan { get; set; }
+}
