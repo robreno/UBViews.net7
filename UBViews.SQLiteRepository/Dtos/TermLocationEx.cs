cc9bebdf55677144b0bd78a630282b8a2859d744
robreno <robreno@hotmail.com>
2023-09-26 06:50:46 -0700

    Changed renamed Length field to TextLength.

--- UBViews.SQLiteRepository/Dtos/TermLocationEx.cs
+++ UBViews.SQLiteRepository/Dtos/TermLocationEx.cs
@@ -1,10 +1,13 @@
 ﻿namespace UBViews.SQLiteRepository.Dtos;
-public class TermLocation
+public class TermLocationEx
 {
-    public string Term { get; set; }
+    public int Id { get; set; }
+    public int QueryResultId { get; set; }
     public int DocumentId { get; set; }
     public int SequenceId { get; set; }
     public int DocumentPosition { get; set; }
     public int TextPosition { get; set; }
     public int TextLength { get; set; }
+    public string Term { get; set; }
+    public string ParagraphId { get; set; }
 }
