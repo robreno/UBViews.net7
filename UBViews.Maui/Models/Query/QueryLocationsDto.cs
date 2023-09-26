dd49c2cac82ab3f2b5ea70140f8fd68ece0b871c
robreno <robreno@hotmail.com>
2023-09-26 06:43:01 -0700

    Updated QueryLocationsDto to match FSharp repository Models.

--- UBViews.Maui/Models/Query/QueryLocationsDto.cs
+++ UBViews.Maui/Models/Query/QueryLocationsDto.cs
@@ -1,7 +1,13 @@
-﻿namespace UBViews.Models.Query
+﻿namespace UBViews.Models.Query;
+public class QueryLocationsDto
 {
-    public class QueryLocationsDto
-    {
-        public List<QueryLocationDto> Locations { get; set; }
-    }
+    public int Id { get; set; }
+    public int Hits { get; set; }
+    public string Type { get; set; }
+    public string Terms { get; set; }
+    public string Proximity { get; set; }
+    public string QueryString { get; set; }
+    public string ReverseQueryString { get; set; }
+    public string QueryExpression { get; set; }
+    public List<QueryLocationDto> QueryLocations { get; set; } = new();
 }
