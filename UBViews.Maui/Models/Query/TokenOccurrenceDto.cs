cbe15c32dfc7855b6bc834eb6182b46bd808d40d
robreno <robreno@hotmail.com>
2023-09-26 06:44:49 -0700

    Added Id and ParagraphId fields to class.

--- UBViews.Maui/Models/Query/TokenOccurrenceDto.cs
+++ UBViews.Maui/Models/Query/TokenOccurrenceDto.cs
@@ -1,10 +1,12 @@
 ﻿namespace UBViews.Models.Query;
 public class TokenOccurrenceDto
 {
+    public int Id { get; set; }
     public int PostingId { get; set; }
     public int DocumentId { get; set; }
     public int SequenceId { get; set; }
     public int SectionId { get; set; }
     public int DocumentPosition { get; set; }
     public int TextPosition { get; set; }
+    public string ParagaphId { get; set; }
 }
