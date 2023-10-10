2d0322f9794f3188b8a6170f7c42a21b453feb4a
robreno <robreno@hotmail.com>
2023-10-10 11:29:58 -0700

    Added check for empty of null string in QueryFilter.

--- QueryFilter/QueryFilter.fs
+++ QueryFilter/QueryFilter.fs
@@ -24,53 +24,77 @@ module QueryFilterService =
 
     // TODO: Create Regex and get match and capture
     let invalidCharStrings = set [| "/"; "\\"; "?"; "~ "; "/"; "\\"; "?";|]
-    let containsInvalidChars (qs : string) =
-        invalidCharStrings
-        |> Seq.map(fun c -> qs.Contains(c))
-        |> Seq.contains(true)
+    let containsInvalidChars (queryString : string) : bool =
+        let isNullOrEmptyQueryString = String.IsNullOrEmpty(queryString)
+        if (isNullOrEmptyQueryString) then
+            true
+        else
+            invalidCharStrings
+            |> Seq.map(fun c -> queryString.Contains(c))
+            |> Seq.contains(true)
 
-    let validQuery (queryString : string) = 
-        let queryStringSet = queryString.ToCharArray() |> Set.ofArray
-        let retval = queryStringSet
+    let validQuery (queryString : string): bool = 
+        let isNullOrEmptyQueryString = String.IsNullOrEmpty(queryString)
+        if (isNullOrEmptyQueryString) then
+            false
+        else
+            let queryStringSet = queryString.ToCharArray() |> Set.ofArray
+            let retval = queryStringSet
+                         |> Seq.map(fun e -> allVaidCharsSet.Contains e)
+                         |> Seq.contains(false)
+            if retval then false
+            else true
+
+    let invalidChars (queryString : string) : char list = 
+        let isNullOrEmptyQueryString = String.IsNullOrEmpty(queryString)
+        if (isNullOrEmptyQueryString) then
+            []
+        else
+            let queryStringSet = queryString.ToCharArray() |> Set.ofArray
+            let charList = queryStringSet
+                           |> Seq.filter(fun e -> not (allVaidCharsSet.Contains e))
+                           |> Seq.toList
+            charList
+
+    let checkForInvalidChars (queryString : string) : bool * string list =
+        let isNullOrEmptyQueryString = String.IsNullOrEmpty(queryString)
+        if (isNullOrEmptyQueryString) then
+            (false, [])
+        else
+            let v1 = invalidCharStrings
+                     |> Seq.map(fun c -> queryString.Contains(c))
+                     |> Seq.contains(true)
+            let v2 = invalidCharStrings
+                     |> Seq.filter(fun c -> queryString.Contains(c))
+                     |> Seq.toList
+            (v1, v2)
+
+    let checkForValidChars (queryString : string) : bool * char list =
+        let isNullOrEmptyQueryString = String.IsNullOrEmpty(queryString)
+        if (isNullOrEmptyQueryString) then
+            (false, [])
+        else
+            let queryStringSet = queryString.ToCharArray() |> Set.ofArray
+            let v1 = queryStringSet
                      |> Seq.map(fun e -> allVaidCharsSet.Contains e)
                      |> Seq.contains(false)
-        if retval then false
-        else true
+            let v2 = queryStringSet
+                     |> Seq.filter(fun e -> not (allVaidCharsSet.Contains e))
+                     |> Seq.toList
+            (not v1,v2)
 
-    let invalidChars (queryString : string) = 
-        let queryStringSet = queryString.ToCharArray() |> Set.ofArray
-        let charList = queryStringSet
-                       |> Seq.filter(fun e -> not (allVaidCharsSet.Contains e))
-                       |> Seq.toList
-        charList
-
-    let checkForInvalidChars (queryString : string) =
-        let v1 = invalidCharStrings
-                 |> Seq.map(fun c -> queryString.Contains(c))
-                 |> Seq.contains(true)
-        let v2 = invalidCharStrings
-                 |> Seq.filter(fun c -> queryString.Contains(c))
-                 |> Seq.toList
-        (v1, v2)
-
-    let checkForValidChars (queryString : string) =
-        let queryStringSet = queryString.ToCharArray() |> Set.ofArray
-        let v1 = queryStringSet
-                 |> Seq.map(fun e -> allVaidCharsSet.Contains e)
-                 |> Seq.contains(false)
-        let v2 = queryStringSet
-                 |> Seq.filter(fun e -> not (allVaidCharsSet.Contains e))
-                 |> Seq.toList
-        (not v1,v2)
-
-    let checkForValidForm (queryString : string) =
-        let v1 = invalidStringsSet
-                 |> Seq.map(fun s -> queryString.Contains(s))
-                 |> Seq.contains(true)
-        let v2 = invalidStringsSet
-                 |> Seq.filter(fun s -> queryString.Contains(s))
-                 |> Seq.toList
-        (not v1,v2)
+    let checkForValidForm (queryString : string) : bool * string list =
+        let isNullOrEmptyQueryString = String.IsNullOrEmpty(queryString)
+        if (isNullOrEmptyQueryString) then
+            (false, [])
+        else
+            let v1 = invalidStringsSet
+                     |> Seq.map(fun s -> queryString.Contains(s))
+                     |> Seq.contains(true)
+            let v2 = invalidStringsSet
+                     |> Seq.filter(fun s -> queryString.Contains(s))
+                     |> Seq.toList
+            (not v1,v2)
 
     let checkQuery (queryString : string) =
         let lst = list.Empty
