c628afc2d329909b012e307db32920ce1217807c
robreno <robreno@hotmail.com>
2023-11-05 07:22:12 -0800

    Added IContactsService interface.

---
+++ UBViews.Maui/Services/IContactsService.cs
@@ -0,0 +1,11 @@
+﻿namespace UBViews.Services;
+
+using UBViews.Models.AppData;
+using System.Xml.Linq;
+
+public interface IContactsService
+{
+    Task<int> SaveContactAsync(ContactDto contact);
+    Task SaveContactsAsync();
+    Task<List<ContactDto>> GetContactsAsync();
+}
