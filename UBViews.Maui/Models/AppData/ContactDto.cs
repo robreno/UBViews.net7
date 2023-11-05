045b3954d4a498a9a82448f38565a2f3c9815db0
robreno <robreno@hotmail.com>
2023-11-04 21:51:40 -0700

    Added ContactDto class.

---
+++ UBViews.Maui/Models/AppData/ContactDto.cs
@@ -0,0 +1,17 @@
+﻿namespace UBViews.Models.AppData;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ContactDto
+{
+    public int Id { get; set; }
+    public bool AutoSendEmail { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string DisplayName { get; set; }
+    public string Email {  get; set; }
+}
