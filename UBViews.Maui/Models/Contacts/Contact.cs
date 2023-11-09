0a321fc63fa8ea82101bcd6d141c97b7c9e34655
robreno <robreno@hotmail.com>
2023-11-09 08:36:48 -0800

    Added Contacts model folder and Contact class.

---
+++ UBViews.Maui/Models/Contacts/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace UBViews.Models.Contacts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public bool AutoSendEmail { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string DisplayName { get; set; }
+    public string Email {  get; set; }
+}
