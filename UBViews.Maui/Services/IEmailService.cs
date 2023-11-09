2b5fd81744c817d69d545076f9143264c0e8fb9f
robreno <robreno@hotmail.com>
2023-11-09 10:12:30 -0800

    Updated IEmailSerivce interface with methods.

--- UBViews.Maui/Services/IEmailService.cs
+++ UBViews.Maui/Services/IEmailService.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿namespace UBViews.Services;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UBViews.Services;
+using UBViews.Models.AppData;
 public interface IEmailService
 {
-    Task<string> CreateEmailText(string pretext, string postText, string subject, List<string> recipients, EmailBodyFormat bodyFormat = EmailBodyFormat.PlainText);
+    Task<List<ContactDto>> GetContactsAsync();
+    Task SetPreTextAsync(string preText);
+    Task SetPostTextAsync(string postText);
+    Task SetRecipientsAsync(List<string> recipients);
+    Task SetSubjectAsync(string subject);
+    Task<string> CreateEmailTextAsync(string pretext, string postText, string subject, List<string> recipients, EmailBodyFormat bodyFormat = EmailBodyFormat.PlainText);
+    Task<string> CreatePlainTextEmailAsync(string pretext, string postText, string subject, List<string> recipients);
+    Task<string> CreateHtmlTextEmailAsync(string pretext, string postText, string subject, List<string> recipients);
 }
