b469f665ef916143c8596b0c18f998ef198247db
robreno <robreno@hotmail.com>
2023-11-09 07:27:38 -0800

    Added IEmailService interface for future development.

---
+++ UBViews.Maui/Services/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBViews.Services;
+public interface IEmailService
+{
+    Task<string> CreateEmailText(string pretext, string postText, string subject, List<string> recipients, EmailBodyFormat bodyFormat = EmailBodyFormat.PlainText);
+}
