52406bbe07c726967841c9b9a57cdc72b7c9d5d5
robreno <robreno@hotmail.com>
2023-11-17 18:28:35 -0800

    Added fields to Dto

--- UBViews.Maui/Models/AppData/AppFileDto.cs
+++ UBViews.Maui/Models/AppData/AppFileDto.cs
@@ -2,6 +2,10 @@
 public class AppFileDto
 {
     public int Id { get; set; }
-    public string FilePath { get; set; }
-    public string FileName { get; set; }
+    public string Name { get; set; }
+    public string Folder { get; set; }
+    public string Path { get; set; }
+    public long Length { get; set; }
+    public string Size { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
 }
