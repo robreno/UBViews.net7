032e4b49a9c1096bbc3368e76d4c8505b1fae9c9
robreno <robreno@hotmail.com>
2023-11-26 08:28:01 -0800

    Added Async methods. Error message instrumentation.

--- UBViews.Maui/Models/Audio/AudioMarkerSequence.cs
+++ UBViews.Maui/Models/Audio/AudioMarkerSequence.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Xml.Linq;
+﻿namespace UBViews.Models.Audio;
 
-namespace UBViews.Models.Audio;
+using System.Collections.Generic;
+using System.Xml.Linq;
 
 /// <summary>
 /// An ordered collection of PlaybackMediaMarker objects.
 /// </summary>
 public class AudioMarkerSequence
 {
+    private readonly string _className = "AudioMarkerSequence";
+
     /// <summary>
     /// SortedList of MedidaMarkers
     /// </summary>
@@ -25,7 +27,37 @@ public class AudioMarkerSequence
     /// <returns></returns>
     public AudioMarker GetAt(int index)
     {
-        return markers[index];
+        string _methodName = "GetAt";
+
+        try
+        {
+            return markers[index];
+        }
+        catch (Exception ex)
+        {
+            string innerMessage = $"Exception raised in {_className}.{_methodName} => {ex.Message}";
+            throw new Exception(innerMessage);
+        }
+    }
+
+    /// <summary>
+    /// Returns the number of items in the sequence.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public async Task<AudioMarker> GetAtAsync(int index)
+    {
+        string _methodName = "GetAtAsync";
+
+        try
+        {
+            return markers[index];
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return null;
+        }
     }
 
     /// <summary>
@@ -35,30 +67,124 @@ public class AudioMarkerSequence
     /// <returns></returns>
     public AudioMarker GetBySeqId(int seqId)
     {
-        if (seqId < 1)
-        {
-            return null;
-        }
+        string _methodName = "GetBySeqId";
 
-        int index = seqId - 1;
-        return markers[index];
+        try
+        {
+            if (seqId < 1)
+            {
+                return null;
+            }
+
+            int index = seqId - 1;
+            return markers[index];
+        }
+        catch (Exception ex)
+        {
+            string innerMessage = $"Exception raised in {_className}.{_methodName} => {ex.Message}";
+            throw new Exception(innerMessage);
+        }
     }
 
     /// <summary>
-    /// Removes all elements from the sequence.
+    /// GetBySeqIdAsync
+    /// </summary>
+    /// <param name="seqId"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public async Task<AudioMarker> GetBySeqIdAsync(int seqId)
+    {
+        string _methodName = "GetBySeqIdAsync";
+
+        try
+        {
+            if (seqId < 1)
+            {
+                return null;
+            }
+
+            int index = seqId - 1;
+            return markers[index];
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Clear Removes all elements from the sequence.
     /// </summary>
     public void Clear()
     {
-        markers.Clear();
+        string _methodName = "Clear";
+
+        try
+        {
+            markers.Clear();
+        }
+        catch (Exception ex)
+        {
+            string innerMessage = $"Exception raised in {_className}.{_methodName} => {ex.Message}";
+            throw new Exception(innerMessage);
+        }
     }
 
     /// <summary>
-    /// 
+    /// ClearAsync
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public async Task ClearAsync()
+    {
+        string _methodName = "ClearAsync";
+
+        try
+        {
+            markers.Clear();
+            return;
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Insert ...
     /// </summary>
     /// <param name="mediaMarker"></param>
     public void Insert(AudioMarker mediaMarker)
     {
-        markers.Add(Size, mediaMarker);
+        string _methodName = "Insert";
+
+        try
+        {
+            markers.Add(Size, mediaMarker);
+        }
+        catch (Exception ex)
+        {
+            string innerMessage = $"Exception raised in {_className}.{_methodName} => {ex.Message}";
+            throw new Exception(innerMessage);
+        }
+    }
+
+    public async Task InsertAsync(AudioMarker mediaMarker)
+    {
+        string _methodName = "InsertAsync";
+
+        try
+        {
+            markers.Add(Size, mediaMarker);
+            return;
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return;
+        }
     }
 
     /// <summary>
@@ -67,7 +193,36 @@ public class AudioMarkerSequence
     /// <returns></returns>
     public IList<AudioMarker> Values()
     {
-        return markers.Values;
+        string _methodName = "Values";
+
+        try
+        {
+            return markers.Values;
+        }
+        catch (Exception ex)
+        {
+            string innerMessage = $"Exception raised in {_className}.{_methodName} => {ex.Message}";
+            throw new Exception(innerMessage);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IList<AudioMarker>> ValuesAsync()
+    {
+        string _methodName = "ValuesAsync";
+
+        try
+        {
+            return markers.Values;
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return null;
+        }
     }
 }
 
