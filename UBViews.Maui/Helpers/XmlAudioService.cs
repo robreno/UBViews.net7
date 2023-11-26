5a98750fb4f2ecd3ecbf858485467003c2ec786b
robreno <robreno@hotmail.com>
2023-11-26 10:57:54 -0800

    Added Dictionary for filtering Astrisk AudioMarkers.

--- UBViews.Maui/Helpers/XmlAudioService.cs
+++ UBViews.Maui/Helpers/XmlAudioService.cs
@@ -12,20 +12,40 @@ using UBViews.Models.Audio;
 
 public partial class XmlAudioService : IAudioService
 {
+    #region  Private Data
     /// <summary>
     /// 
     /// </summary>
     private IFileService fileService;
 
+    private Dictionary<int, List<int>> _astriskDic = new Dictionary<int, List<int>>()
+    {
+        { 31, new List<int> { 92 } },
+        { 56, new List<int> { 92 } },
+        { 120, new List<int> { 41 } },
+        { 134, new List<int> { 70 } },
+        { 196, new List<int> { 78 } },
+        { 144, new List<int> { 70, 84, 97, 113, 132, 146 } }
+    };
+
     private readonly string _className = "XmlAudioService";
 
-    internal AudioMarkerSequence audioMarkerSequence { get; set; } = new();
+    //Dictionary<string, string> _tildes = new Dictionary<string, string>(
+    //    {
+    //        { "", ""},
+    //    });
 
+    internal AudioMarkerSequence audioMarkerSequence { get; set; } = new();
+    #endregion
+
+    #region  Constructor
     public XmlAudioService(IFileService fileService) 
     {
         this.fileService = fileService;
     }
+    #endregion
 
+    #region  Public Methods
     /// <summary>
     /// Get MediaMarker at index position.
     /// </summary>
@@ -129,6 +149,8 @@ public partial class XmlAudioService : IAudioService
 
         try
         {
+            List<int> astriskSeqIds = new List<int>();
+            bool isAstriskPaper = _astriskDic.TryGetValue(paperId, out astriskSeqIds);
             var fileName = paperId.ToString("000") + ".audio.xml";
             var content = await fileService.LoadAsset("AudioMarkers", fileName);
             var xDoc = XDocument.Parse(content);
@@ -136,6 +158,14 @@ public partial class XmlAudioService : IAudioService
             var markers = root.Descendants("Marker");
             foreach (var marker in markers)
             {
+                int seqId = Int32.Parse(marker.Attribute("seqId").Value);
+                if (isAstriskPaper)
+                {
+                    if (astriskSeqIds.Contains(seqId))
+                    {
+                        continue;
+                    }
+                }
                 var newMarker = new AudioMarker(marker);
                 audioMarkerSequence.Insert(newMarker);
             }
@@ -147,4 +177,5 @@ public partial class XmlAudioService : IAudioService
             return null;
         }
     }
+    #endregion
 }
