53bef41991b080dcd24e6ff1bd4e535ade4506ee
robreno <robreno@hotmail.com>
2023-11-26 06:55:28 -0800

    Added error message instrumentation and file scoped.

--- UBViews.Maui/Helpers/AudioService.cs
+++ UBViews.Maui/Helpers/AudioService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿namespace UBViews.Helpers;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,31 +8,61 @@ using System.Threading.Tasks;
 using UBViews.Services;
 
 using UBViews.Models.Audio;
-
-namespace UBViews.Helpers
+public partial class AudioService
 {
-    public partial class AudioService
-    {
-        IAudioService audioService;
-        public AudioService(IAudioService audioService)
-        {
-            this.audioService = audioService;
-        }
+    IAudioService audioService;
 
-        public async Task<AudioMarker> GetAt(int index)
+    private readonly string _className = "AudioService";
+    public AudioService(IAudioService audioService)
+    {
+        this.audioService = audioService;
+    }
+
+    public async Task<AudioMarker> GetAtAsync(int index)
+    {
+        string _methodName = "GetAtAsync";
+
+        try
         {
             var marker = await audioService.GetAt(index);
             return marker;
         }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return null;
+        }
+    }
 
-        public async Task Clear()
+    public async Task ClearAsync()
+    {
+        string _methodName = "ClearAsync";
+
+        try
         {
             await audioService.Clear();
+            return;
         }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return;
+        }
+    }
 
-        public async Task Insert(AudioMarker mediaMarker)
+    public async Task InsertAsync(AudioMarker mediaMarker)
+    {
+        string _methodName = "InsertAsync";
+
+        try
         {
             await audioService.Insert(mediaMarker);
+            return;
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert($"Exception raised in {_className}.{_methodName} => ", ex.Message, "Ok");
+            return;
         }
     }
 }
