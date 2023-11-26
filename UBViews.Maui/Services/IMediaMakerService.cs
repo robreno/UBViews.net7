b2693dcc96fbb825920eabf6f3f4228ba46301fc
robreno <robreno@hotmail.com>
2023-11-26 06:11:47 -0800

    Added Interface for later development. Created editconfig for file scoped namespaces. But may only work in .NET 8.

---
+++ UBViews.Maui/Services/IMediaMakerService.cs
@@ -0,0 +1,41 @@
+﻿namespace UBViews.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UBViews.Models.Audio;
+public interface IMediaMakerService
+{
+    /// <summary>
+    /// Get MediaMarker at index position.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    Task<AudioMarker> GetAt(int index);
+
+    /// <summary>
+    /// Removes all elements from the sequence.
+    /// </summary>
+    Task Clear();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="audioMarker"></param>
+    Task Insert(AudioMarker audioMarker);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    Task<IList<AudioMarker>> Values();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    Task<AudioMarkerSequence> LoadAudioMarkers(int paperId);
+}
