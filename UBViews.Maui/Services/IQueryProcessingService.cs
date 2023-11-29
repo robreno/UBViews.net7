3a242ca30dd0989a2520d0a5a3930202fcb13f77
robreno <robreno@hotmail.com>
2023-11-28 22:13:46 -0800

    New interface, see impl

---
+++ UBViews.Maui/Services/IQueryProcessingService.cs
@@ -0,0 +1,23 @@
+﻿namespace UBViews.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QueryEngine;
+using UBViews.Models.Query;
+
+public interface IQueryProcessingService
+{
+    Task<bool> ParseQueryAsync(string queryString);
+    Task<bool> RunQueryAsync(string queryString);
+    Task<(bool, QueryResultDto)> QueryResultExistsAsync(string queryString);
+    Task<SimpleEnumeratorsEx.TokenPostingList> CreateTokenPostingListAsync(string trm, List<TokenOccurrenceDto> toks);
+    Task<bool> GetQueryResultExistsAsync();
+    Task<string> GetQueryInputStringAsync();
+    Task<string> GetQueryExpressionAsync();
+    Task<List<string>> GetTermListAsync();
+    Task<QueryResultLocationsDto> GetQueryResultLocationsAsync();
+}
