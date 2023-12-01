d2cac0d65f121dd1f67fb08f2cfa333347967698
robreno <robreno@hotmail.com>
2023-12-01 09:26:43 -0800

    Added QueryType

---
+++ UBViews.Maui/Models/Query/QueryType.cs
@@ -0,0 +1,18 @@
+﻿namespace UBViews.Models.Query;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class QueryType
+{
+    public string BaseQuery { get; set; } = null;
+    public string FilterByOp { get; set; } = null;
+    public string FilterByPostfixOp { get; set; } = null;
+    public string QueryString { get; set; } = null;
+    public string ReverseQuery { get; set; } = null;
+    public string Type { get; set; } = null;
+    public int Length { get; set; } = 0;
+}
