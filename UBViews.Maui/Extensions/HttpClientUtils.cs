66712522b8976d904942027f97be260699aa75d2
robreno <robreno@hotmail.com>
2023-12-09 10:50:46 -0800

    Added extension methods for HttpClient

---
+++ UBViews.Maui/Extensions/HttpClientUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace UBViews.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class HttpClientUtils
+{
+	public static async Task DownloadFileTaskAsync(this HttpClient client, Uri uri, string FileName)
+	{
+		using (var s = await client.GetStreamAsync(uri))
+		{
+			using (var fs = new FileStream(FileName, FileMode.CreateNew))
+			{
+				await s.CopyToAsync(fs);
+			}
+		}
+	}
+}
---
+++ UBViews.Maui/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace UBViews.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class HttpContentExtensions
+{
+    public static Task ReadAsFileAsync(this HttpContent content, string filename, bool overwrite)
+    {
+        string pathname = Path.GetFullPath(filename);
+        if (!overwrite && File.Exists(filename))
+        {
+            throw new InvalidOperationException(string.Format("File {0} already exists.", pathname));
+        }
+
+        FileStream fileStream = null;
+        try
+        {
+            fileStream = new FileStream(pathname, FileMode.Create, FileAccess.Write, FileShare.None);
+            return content.CopyToAsync(fileStream).ContinueWith(
+                (copyTask) =>
+                {
+                    fileStream.Close();
+                });
+        }
+        catch
+        {
+            if (fileStream != null)
+            {
+                fileStream.Close();
+            }
+            throw;
+        }
+    }
+}
