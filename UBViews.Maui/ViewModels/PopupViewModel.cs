632e1cabec3052cb0231120c5a4ea5d5cc91f32d
robreno <robreno@hotmail.com>
2023-12-13 17:29:49 -0800

    Added Help Popup and PopupViewModel

---
+++ UBViews.Maui/ViewModels/PopupViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace UBViews.ViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Maui.Views;
+
+public partial class PopupViewModel : BaseViewModel
+{
+    public Popup popupPage;
+
+    [RelayCommand]
+    async Task ClosePopup(object obj)
+    {
+        try
+        {
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                if (popupPage != null)
+                {
+                    popupPage.Close();
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert("Exception raised in MainViewModel.NavigateTo => ",
+                ex.Message, "Cancel");
+        }
+    }
+}
