ebe6c23f9b105362fd1e6237951db24b72be1f47
robreno <robreno@hotmail.com>
2023-12-27 08:34:28 -0800

    Added fields to Run
    public string TextSize { get; set; }
    public string FontFamily { get; set; }
    public string TextColor { get; set; }
    public string TextTransform { get; set; }

--- UBViews.Maui/Models/Ubml/Run.cs
+++ UBViews.Maui/Models/Ubml/Run.cs
@@ -3,4 +3,8 @@ public class Run
 {
     public string Text { get; set; }
     public string Style { get; set; }
+    public string TextSize { get; set; }
+    public string FontFamily { get; set; }
+    public string TextColor { get; set; }
+    public string TextTransform { get; set; }
 }
