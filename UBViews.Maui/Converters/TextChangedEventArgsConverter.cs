c74d7faf989a6e731be4f33386d27f6c32e443cf
robreno <robreno@hotmail.com>
2024-01-12 21:25:15 -0800

    Added converter

---
+++ UBViews.Maui/Converters/TextChangedEventArgsConverter.cs
@@ -0,0 +1,38 @@
+﻿namespace UBViews.Converters;
+
+using System.Globalization;
+using CommunityToolkit.Maui.Core.Primitives;
+
+using UBViews.Models;
+
+public class TextChangedEventArgsConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+        
+        var args = (TextChangedEventArgs)value;
+        var newText = args.NewTextValue;
+        var oldText = args.OldTextValue;
+        var editor = GetParameter(parameter);
+        //var dto = new TextChangedEventArgsDto() 
+        //{ 
+        //    Sender = editor, 
+        //    NewText = newText, 
+        //    OldText = oldText, 
+        //    Parameter = parameter 
+        //};
+        return value;
+    }
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+        throw new NotImplementedException();
+	}
+
+    Editor GetParameter(object parameter)
+    {
+        if (parameter is Editor)
+            return (Editor)parameter;
+        return null;
+    }
+}
