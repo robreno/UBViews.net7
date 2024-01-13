1e580bde1c47d01ebe9e963f5fbbb45e9fc98c0a
robreno <robreno@hotmail.com>
2024-01-13 13:10:39 -0800

    Synced

--- UBViews.Maui/Services/INoteService.cs
+++ UBViews.Maui/Services/INoteService.cs
@@ -6,11 +6,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using UBViews.Models;
 using UBViews.Models.Notes;
+using UBViews.Models.Ubml;
 
 public interface INoteService
 {
+    Task<bool> IsDirtyAsync();
+    Task<int> LastNoteIdAsync();
+    Task<List<NoteEntry>> GetNotesAsync();
     Task<NoteLocationsDto> GetNoteLocationsDtoAsync();
+    Task<List<PaperDto>> GetPaperDtosAsync();
+    Task<PaperDto> GetPaperDtoAsync(int id);
+    Task<Paragraph> GetParagraphAsync(int paperId, int seqId);
     Task<Border> CreateNoteBorderAsync(NoteEntry note);
-    Task<List<NoteEntry>> LoadNotesAsync();
 }
