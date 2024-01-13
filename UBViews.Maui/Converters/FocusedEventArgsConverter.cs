504a9c1bef35b6420b53eb22e34f5d1b340c57bf
robreno <robreno@hotmail.com>
2024-01-12 21:20:36 -0800

    Added converter

---
+++ UBViews.Maui/Converters/FocusedEventArgsConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace UBViews.Converters;
+
+using System.Globalization;
+using CommunityToolkit.Maui.Core.Primitives;
+
+using UBViews.Models;
+
+public class FocusedEventArgsConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+        
+        var args = (FocusEventArgs)value;
+        var editor = (Editor)args.VisualElement;
+        var id = editor.StyleId;
+        var name = editor.ClassId;
+        var dto = new FocusedEventArgsDto();
+        return value;
+    }
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+        throw new NotImplementedException();
+	}
+}
