e8acc0bb3858c29c6d48258f18b4b74152f87c2c
robreno <robreno@hotmail.com>
2024-01-12 22:20:23 -0800

    Added EqualTo

--- UBViews.Maui/Models/Notes/NoteEntry.cs
+++ UBViews.Maui/Models/Notes/NoteEntry.cs
@@ -21,4 +21,30 @@ public class NoteEntry
     public string Text { get; set; }
     public string Style { get; set; }
     public List<NoteText> NoteEntries { get; set; } = new();
+
+    public bool EqualTo(NoteEntry dto)
+    {
+        bool isEqual = false;
+        if (dto == null) { return false; }
+        else
+        {
+            if (dto.Id == this.Id &&
+                dto.Type == this.Type &&
+                dto.PaperId == this.PaperId &&
+                dto.SequenceId == this.SequenceId &&
+                dto.LocationId == this.LocationId &&
+                dto.Pid == this.Pid &&
+                dto.DateCreated == this.DateCreated &&
+                dto.DateEdited == this.DateEdited &&
+                dto.Author == this.Author &&
+                dto.Subject == this.Subject &&
+                dto.Text == this.Text &&
+                dto.Style == this.Style &&
+                dto.NoteRuns == this.NoteRuns)
+            {
+                isEqual = true;
+            }
+        }
+        return isEqual;
+    }
 }
