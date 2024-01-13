5ef698810c0c251032e88700e3f9352240e9c15e
robreno <robreno@hotmail.com>
2024-01-12 21:23:19 -0800

    Added converter

---
+++ UBViews.Maui/Converters/LoadedEventArgsConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace UBViews.Converters;
+
+using System.Globalization;
+using CommunityToolkit.Maui.Core.Primitives;
+using CommunityToolkit.Mvvm.Input;
+
+public class LoadedEventArgsConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+        var args = (EventArgs)value;
+        var editor = GetParameter(parameter);
+        return value;
+    }
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+        throw new NotImplementedException();
+	}
+
+    Editor GetParameter(object parameter)
+    {
+        if (parameter is Editor)
+            return (Editor)parameter;
+        return null;
+    }
+}
