ebb8c4be0ce5d234543704ffdbd420fdb401d8db
robreno <robreno@hotmail.com>
2024-01-13 13:53:26 -0800

    Added Trigger

---
+++ UBViews.Maui/Triggers/NumericValidationTriggerAction.cs
@@ -0,0 +1,10 @@
+﻿namespace UBViews.Triggers;
+public class NumericValidationTriggerAction : TriggerAction<Entry>
+{
+    protected override void Invoke(Entry entry)
+    {
+        double result;
+        bool isValid = Double.TryParse(entry.Text, out result);
+        entry.TextColor = isValid ? Colors.Black : Colors.Red;
+    }
+}
