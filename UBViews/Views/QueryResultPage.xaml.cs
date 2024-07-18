4f15d4ab99e198931cd39b107fa3c4729448f649
robreno <robreno@hotmail.com>
2024-07-18 07:19:14 -0700

    Removed unneeded code.

--- UBViews/Views/QueryResultPage.xaml.cs
+++ UBViews/Views/QueryResultPage.xaml.cs
@@ -10,16 +10,4 @@ public partial class QueryResultPage : ContentPage
         BindingContext = vm;
         vm.contentPage = this;
     }
-
-    public async void ScrollToLabel(string name)
-    {
-        Label targetLabel = this.FindByName(name) as Label;
-        await contentScrollView.ScrollToAsync(targetLabel, ScrollToPosition.Start, false);
-    }
-
-    //private void ContentPage_Loaded(object sender, EventArgs e)
-    //{
-    //    Label targetLabel = this.FindByName("RID_0_1_0") as Label;
-    //    this.contentScrollView.ScrollToAsync(targetLabel, ScrollToPosition.Start, false);
-    //}
 }
